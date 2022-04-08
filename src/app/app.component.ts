593d05d2f6762c2c5e83cde04f79191a6eacf9bd
Noran Diaconu <norandiaconu@gmail.com>
2022-04-08 19:17:17 -0400

    Updated page sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,31 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger(
+      "fade",
+      [
+        transition(
+          ":enter",
+          [
+            style({ opacity: 0 }),
+            animate("0.5s ease-out", style({ opacity: 1 }))
+          ]
+        ),
+        transition(
+          ":leave",
+          [
+            style({ opacity: 1 }),
+            animate("0.5s ease-in", style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class AppComponent {
   title = 'noran-github-io';
